254c85a9b997f1d17c0fefbbb56a7bc545cab391
francesco.casimiro@gmail.com <francesco.casimiro@gmail.com@acc8e4ea-a03e-16b5-9b3a-7747b85e903c>
2011-05-18 14:52:18 +0000

    Upload Riepilogo

---
+++ team19/Model/Riepilogo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Team19.Model
+{
+    public interface IRiepilogo
+    {
+        public List<Currency> GetImportiPagati();
+        public List<Currency> GetImportiDaPagare();
+    }
+
+    public class RiepilogoCliente : IRiepilogo
+    {
+        #region IRiepilogo Members
+
+        public List<Currency> GetImportiPagati()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Currency> GetImportiDaPagare()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+
+    public class RiepilogoFornitore : IRiepilogo
+    {
+        #region IRiepilogo Members
+
+        public List<Currency> GetImportiPagati()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Currency> GetImportiDaPagare()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+
+}
