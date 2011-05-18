c43f572b7c8bf9db54b973eb2d7c84d8c285153a
francesco.casimiro@gmail.com <francesco.casimiro@gmail.com@acc8e4ea-a03e-16b5-9b3a-7747b85e903c>
2011-05-18 14:53:33 +0000

    Upload Sorgente e Destinazione

---
+++ team19/Model/Sorgente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Team19.Model
+{
+    public interface Sorgente
+    {
+        String getInfo();
+    }
+
+    public interface Destinazione
+    {
+        String getInfo();
+    }
+}
