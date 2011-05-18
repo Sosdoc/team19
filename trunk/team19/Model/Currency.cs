39efc845cf1046b1ab15081507336cd60ca48e04
francesco.casimiro@gmail.com <francesco.casimiro@gmail.com@acc8e4ea-a03e-16b5-9b3a-7747b85e903c>
2011-05-18 14:50:56 +0000

    Upload Currency

---
+++ trunk/team19/Model/Currency.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Team19.Model
+{
+    class Currency
+    {
+        private double _value;
+
+        public Currency(double value)
+        {
+            this._value = value;
+        }
+        public override String ToString()
+        {
+            return "€ " + _value.ToString();
+        }
+    }
+}
