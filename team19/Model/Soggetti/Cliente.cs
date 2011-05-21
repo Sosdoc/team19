f7c944822e39953a59c44092090162c34b9c85d1
francesco.casimiro <francesco.casimiro@acc8e4ea-a03e-16b5-9b3a-7747b85e903c>
2011-05-21 16:55:13 +0000

    Aggiunti Cliente.cs e Fornitore.cs

---
+++ team19/Model/Soggetti/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Team19.Model.Soggetti
+{
+    public class Cliente : Soggetto
+    {
+        private string? _codiceFiscale;
+
+        
+
+        public Cliente(string denominazione, string telefono, string email, string? partitaIva, string? codiceFiscale) :base(denominazione,telefono,email,partitaIva)
+        {
+            if (!partitaIva.HasValue && !codiceFiscale.HasValue)
+                throw new ArgumentNullException("Partita Iva e Codice Fiscale entrambi nulli");
+            _codiceFiscale = codiceFiscale;
+        }
+
+        public string CodiceFiscale  // Chiedere per il null
+        {
+            get { return _codiceFiscale.Value; }
+        }
+    }
+}
---
+++ team19/Model/Soggetti/Indirizzo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Team19.Model.Soggetti
+{
+    public class Indirizzo
+    {
+        private string _via;
+        private string _numeroCivico;
+        private string _località;
+        private string _cap;
+        private string _provincia;
+        private string _nazione;
+
+        public string Via
+        {
+            get { return _via; }
+            set { _via = value; }
+        }
+        
+        public string NumeroCivico
+        {
+            get { return _numeroCivico; }
+        //    set { _numeroCivico = value; }
+        }
+        
+        public string Località
+        {
+            get { return _località; }
+        //    set { _località = value; }
+        }
+        
+        public string Cap
+        {
+            get { return _cap; }
+        //    set { _cap = value; }
+        }
+        
+        public string Provincia
+        {
+            get { return _provincia; }
+        //    set { _provincia = value; }
+        }
+        
+        public string Nazione
+        {
+            get { return _nazione; }
+        //    set { _nazione = value; }
+        }
+
+
+    }
+}
