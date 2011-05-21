706c9c4a67a6b146e78060bb3772128c53f7000a
francesco.casimiro <francesco.casimiro@acc8e4ea-a03e-16b5-9b3a-7747b85e903c>
2011-05-21 16:55:13 +0000

    Aggiunti Cliente.cs e Fornitore.cs

---
+++ trunk/team19/Model/Soggetti/Fornitore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Team19.Model.Soggetti
+{
+    class Fornitore:Soggetto
+    {
+            public Fornitore(string denominazione, string telefono, string email, string? partitaIva)
+                : base(denominazione, telefono, email, partitaIva)
+            {
+                if (!partitaIva.HasValue)
+                    throw new ArgumentNullException("Partita Iva nulla");
+            }
+        
+    }
+}
---
+++ trunk/team19/Model/Soggetti/Soggetto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Team19.Model.Soggetti
+{
+    public abstract class Soggetto
+    {
+        private string _denominazione;
+        private string _telefono;
+        private string _eMail;
+        private string? _partitaIva;
+        private Indirizzo _indirizzo;
+        protected Soggetto(string denominazione, string telefono, string email, string? partitaIva)
+        {
+            _denominazione = denominazione;
+            _telefono = telefono;
+            _eMail = email;
+            _partitaIva = partitaIva;
+        }
+        
+        public Indirizzo Indirizzo
+        {
+            get { return _indirizzo; }
+            //set { _indirizzo = value; }
+        }
+   
+        public string Telefono
+        {
+            get { return _telefono; }
+            //set { _telefono = value; }
+        }
+
+        public string EMail
+        {
+            get { return _eMail; }
+            //set { _eMail = value; }
+        }
+
+        public string Denominazione
+        {
+            get { return _denominazione; }
+            //set { _denominazione = value; }
+        }
+
+        public string PartitaIva // Chiedere per il null
+        {
+            get { return _partitaIva.Value; }
+            //set { _partitaIva = value; }
+        }
+    }
+}
