e3b05df39f275d8c5d0d4ac8eb4b9e3307e69d25
valep27 <valep27@acc8e4ea-a03e-16b5-9b3a-7747b85e903c>
2011-05-22 15:00:12 +0000

    Aggiornato Currency per avere una valuta definita dall'utente(nessun controllo, si accettano stringhe di lunghezza 1, decisamente migliorabile magari usando le costanti di .NET, disponibile anche il vecchio costruttore che mette a default "€" come valuta)
    Aggiunto un metodo per calcolare l'importo nella Cassa, usa query LINQ per recuperare tutti gli importi relativi alla cassa, DA TESTARE.

--- team19/Model/Currency.cs
+++ team19/Model/Currency.cs
@@ -8,21 +8,40 @@ namespace Team19.Model
     public class Currency
     {
         private double _value;
+        private String _valuta;
+
+        public String Valuta
+        {
+            get { return _valuta; }
+            set { _valuta = value; }
+        }
 
         public double Value
         {
             get { return _value; }
-            set { this._value = value; }
         }
 
         public Currency(double value)
+            : this(value, "€")
         {
+        }
+
+        public Currency(double value, String valuta)
+        {
+            if (value <= 0)
+                throw new ArgumentException("value <= 0");
+            if (valuta.Length != 1)
+                throw new ArgumentException("valuta.Length != 1");
+
             this._value = value;
+            this._valuta = valuta;
         }
 
         public override String ToString()
         {
-            return "€ " + this.Value.ToString();
+            return this.Valuta + " " + this.Value.ToString();
         }
+
+        //TODO: override degli operatori + - * / ==
     }
 }
