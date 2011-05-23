ba05ed3ca919d0772167204bbf05128bb2c0a1d6
valep27 <valep27@acc8e4ea-a03e-16b5-9b3a-7747b85e903c>
2011-05-23 13:43:22 +0000

    Aggiornato Dipendente

--- trunk/team19/Model/Dipendente.cs
+++ trunk/team19/Model/Dipendente.cs
@@ -7,31 +7,35 @@ namespace Team19.Model
 {
     public class Dipendente
     {
-        private string username;
-        private string password;
-        private string cognome;
-        private string nome;
+        private string _username;
+        private string _password;
+        private string _cognome;
+        private string _nome;
 
-        //public string Username
-        //{
-        //    get { return username; }
-        //    set { username = value; }
-        //}
-        //public string Password
-        //{
-        //    get { return password; }
-        //    set { password = value; }
-        //}
-        //public string Nome
-        //{
-        //    get { return nome; }
-        //    set { nome = value; }
-        //}
-        //public string Cognome
-        //{
-        //    get { return cognome; }
-        //    set { cognome = value; }
-        //}
+        public Dipendente(string username, string password, string cognome, string nome)
+        {
+            this._username = username;
+            this._password = password;
+            this._cognome = cognome;
+            this._nome = nome;
+        }
+
+        public string Username
+        {
+            get { return _username; }
+        }
+        public string Password
+        {
+            get { return _password; }
+        }
+        public string Nome
+        {
+            get { return _nome; }
+        }
+        public string Cognome
+        {
+            get { return _cognome; }
+        }
         
     }
 }
