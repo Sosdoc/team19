1233f8404503ee1ec4c35a429be25b999da922be
francesco.casimiro <francesco.casimiro@acc8e4ea-a03e-16b5-9b3a-7747b85e903c>
2011-05-23 15:21:12 +0000

    Aggiornamento FatturaFactory.cs (metodo per generare il numero)

--- trunk/team19/Model/FatturaFactory.cs
+++ trunk/team19/Model/FatturaFactory.cs
@@ -9,15 +9,34 @@ namespace Team19.Model
     {
         public static FatturaVendita CreateFatturaVendita(Cliente cliente, DateTime data, List<RigaFattura> elencoProdotti)
         {
-            return new FatturaVendita(cliente, data, Document.GetInstance().NumeroProssimaFatturaDiVendita(data), elencoProdotti);
+            return new FatturaVendita(cliente, data, NumeroProssimaFatturaDiVendita(data), elencoProdotti);
         }
 
         public static FatturaAcquisto CreateFatturaAcquisto(Fornitore fornitore, DateTime data, int numero, Currency importo)
         {
             return new FatturaAcquisto(fornitore, data, numero, importo);
         }
-    }
 
+        private static int NumeroProssimaFatturaDiVendita(DateTime dataNuovaFattura)
+        {
+            int retval = 1;
+
+            //recupero l'ultima fattura emessa disponibile
+            //FatturaVendita ultimaFatturaDiVendita = Document.GetInstance().Fatture.OfType<FatturaVendita>().Last();
+
+            FatturaVendita ultimaFatturaDiVendita =
+                (from fattura in Document.GetInstance().GetFattureVendita()
+                 orderby fattura.Data descending
+                 select fattura).First();
+
+            //se non è cambiato anno dall'ultima fattura
+            if (ultimaFatturaDiVendita.Data.Year == dataNuovaFattura.Year)
+                retval = ultimaFatturaDiVendita.NumeroFattura + 1;
+
+            return retval;
+        }
+    }
+     
     public class FatturaVendita : Fattura, ISorgente
     {
         private List<RigaFattura> _elencoProdotti;
