4f8265f6bee45acf5303cdf5ce97db6bc8fdf438
valep27 <valep27@acc8e4ea-a03e-16b5-9b3a-7747b85e903c>
2011-05-25 14:34:05 +0000

    Aggiunto caching del saldo in ContenitoreDiDenaro

--- team19/Model/ContenitoreDiDenaro.cs
+++ team19/Model/ContenitoreDiDenaro.cs
@@ -5,22 +5,31 @@ using System.Text;
 
 namespace Team19.Model
 {
-    public abstract class ContenitoreDiDenaro : ISorgente, IDestinazione
+    public abstract class ContenitoreDiDenaro : ISorgente, IDestinazione, IDisposable
     {
         private readonly Currency _saldoIniziale;
+        //Cache
+        private Currency _cacheSaldo;
+        //Evento per refresh cache
+        public event EventHandler Changed;
 
         public ContenitoreDiDenaro(Currency saldoIniziale)
         {
             _saldoIniziale = saldoIniziale;
+            _cacheSaldo = null;
+            Document.Changed += Document_Changed;
         }
+
         public string Tipo
         {
             get { return this.GetType().Name; }
         }
+
         public virtual string CodConto
         {
             get { return ""; }
         }
+
         public Currency SaldoIniziale
         {
             get { return _saldoIniziale; }
@@ -30,8 +39,11 @@ namespace Team19.Model
         {
             get //template method, ogni classe concreta deve implementare Equals
             {
+                if (_cacheSaldo != null)
+                    return _cacheSaldo;
+
                 Currency sum = SaldoIniziale;
-                
+
                 IEnumerable<Currency> queryImportiMovimentiConQuestoContenitore =
                     from movimento in Document.GetInstance().Movimenti
                     where this.Equals(movimento.Destinazione)
@@ -48,14 +60,37 @@ namespace Team19.Model
                 foreach (Currency c in queryImportiMovimentiConQuestoContenitore)
                     sum -= c;
 
+                _cacheSaldo = sum;
                 return sum;
             }
         }
-        
+
+        protected virtual void Document_Changed(object sender, EventArgs args)
+        {
+            OnChanged();
+        }
+
+        private void OnChanged()
+        {
+            _cacheSaldo = null;
+            if (Changed != null)
+                Changed(this, EventArgs.Empty);
+        }
+
+
         public abstract override bool Equals(object obj);
+
         public override int GetHashCode()
         {
             return base.GetHashCode();
         }
+
+        #region IDisposable members
+        public void Dispose()
+        {
+            Document.Changed -= Document_Changed;
+        }
+        #endregion
+
     }
 }
