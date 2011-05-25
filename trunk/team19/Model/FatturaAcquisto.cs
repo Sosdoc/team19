e556377caa043054e6dd99c8482cb48839e9fd55
valep27 <valep27@acc8e4ea-a03e-16b5-9b3a-7747b85e903c>
2011-05-25 15:56:42 +0000

    Eliminata FatturaFactory, al suo posto sono stati aggiunti i metodi statici in entrambe le Fatture, aggiornati i riferimenti alla factory in Document.

---
+++ trunk/team19/Model/FatturaAcquisto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Team19.Model
+{
+    public class FatturaAcquisto : Fattura, IDestinazione
+    {
+        private Currency _importo;
+        private Fornitore _fornitore;
+
+        public FatturaAcquisto(Fornitore fornitore, DateTime data, int numero, Currency importo)
+            : base(data, numero)
+        {
+            if (importo.Value <= 0)
+                throw new ArgumentException("importo.Value <= 0");
+            if (fornitore == null)
+                throw new ArgumentNullException("fornitore");
+
+            this._importo = importo;
+            this._fornitore = fornitore;
+        }
+
+        public static FatturaAcquisto CreateFatturaAcquisto(Fornitore fornitore, DateTime data, int numero, Currency importo)
+        {
+            return new FatturaAcquisto(fornitore, data, numero, importo);
+        }
+
+        public Fornitore Fornitore
+        {
+            get { return _fornitore; }
+        }
+
+        public override Currency Importo
+        {
+            get { return _importo; }
+        }
+
+
+    }
+}
