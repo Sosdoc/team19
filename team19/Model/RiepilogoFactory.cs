3c0d38c5040b3f03fe0b0e139feff284cdbea755
francesco.casimiro <francesco.casimiro@acc8e4ea-a03e-16b5-9b3a-7747b85e903c>
2011-05-26 16:12:37 +0000

    Sistemati i riepiloghi (pattern template method + factory) e sistemate le query

--- team19/Model/RiepilogoFactory.cs
+++ team19/Model/RiepilogoFactory.cs
@@ -7,16 +7,119 @@ namespace Team19.Model
 {
     class RiepilogoFactory
     {
-        //static IRiepilogo CreateRiepilogo(Cliente cliente)
-        //{
-        //    return new RiepilogoCliente(cliente);
-        //}
+        public static IRiepilogo CreateRiepilogo(ICliente cliente)
+        {
+            return new RiepilogoCliente(cliente);
+        }
 
-        //static IRiepilogo CreateRiepilogo(Fornitore fornitore)
-        //{
-        //    return new RiepilogoFornitore(fornitore);
-        //}
+        public static IRiepilogo CreateRiepilogo(IFornitore fornitore)
+        {
+            return new RiepilogoFornitore(fornitore);
+        }
 
-       
+        public abstract class Riepilogo : IRiepilogo
+        {
+
+            public Dictionary<int, Currency> GetImportiPagati()
+            {
+                Dictionary<int, Currency> result = new Dictionary<int, Currency>();
+                foreach (Fattura fattura in this.FatturePagate)
+                    result.Add(fattura.NumeroFattura, fattura.Importo);
+                return result;
+            }
+
+            public Dictionary<int, Currency> GetImportiDaPagare()
+            {
+                Dictionary<int, Currency> result = new Dictionary<int, Currency>();
+                foreach (Fattura fattura in this.FattureDaPagare)
+                    result.Add(fattura.NumeroFattura, fattura.Importo);
+                return result;
+            }
+
+            protected abstract IEnumerable<Fattura> FatturePagate
+            {
+                get;
+            }
+            protected abstract IEnumerable<Fattura> FattureDaPagare
+            {
+                get;
+            }
+        }
+
+        private class RiepilogoCliente : Riepilogo
+        {
+            private ICliente _cliente;
+
+            public RiepilogoCliente(ICliente cliente)
+            {
+                _cliente = cliente;
+            }
+
+            public ICliente Cliente
+            {
+                get { return _cliente; }
+            }
+
+            #region Riepilogo Members
+            protected override IEnumerable<Fattura> FatturePagate
+            {
+                get
+                {
+                    IEnumerable<FatturaVendita> fatture =
+                        from fattura in Document.GetInstance().GetFattureVendita()
+                        join movimento in Document.GetInstance().GetIncassiVendite() on fattura equals (FatturaVendita)movimento.Sorgente
+                        where fattura.Cliente.Equals(Cliente)
+                        select fattura;
+                    return fatture;
+                }
+            }
+            protected override IEnumerable<Fattura> FattureDaPagare
+            {
+                get
+                {
+                    return Document.GetInstance().Fatture.OfType<FatturaVendita>().Except(this.FatturePagate);
+                }
+            }
+            #endregion
+        }
+
+        private class RiepilogoFornitore : Riepilogo
+        {
+            private IFornitore _fornitore;
+
+            public RiepilogoFornitore(IFornitore fornitore)
+            {
+                this._fornitore = fornitore;
+            }
+
+            IFornitore Fornitore
+            {
+                get { return _fornitore; }
+            }
+
+            #region Riepilogo Members
+
+            protected override IEnumerable<Fattura> FatturePagate
+            {
+                get
+                {
+                    IEnumerable<FatturaAcquisto> fatture =
+                        from fattura in Document.GetInstance().GetFattureAcquisto()
+                        join movimento in Document.GetInstance().GetPagamentiAcquisti() on fattura equals (FatturaAcquisto)movimento.Destinazione
+                        where fattura.Fornitore.Equals(Fornitore)
+                        select fattura;
+                    return fatture;
+                }
+            }
+            protected override IEnumerable<Fattura> FattureDaPagare
+            {
+                get
+                {
+                    return Document.GetInstance().Fatture.OfType<FatturaAcquisto>().Except(this.FatturePagate);
+                }
+            }
+
+            #endregion
+        }
     }
 }
