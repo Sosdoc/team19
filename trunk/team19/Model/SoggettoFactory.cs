f209b5a7a737a03ba2fddc4b836c9ae925026833
francesco.casimiro <francesco.casimiro@acc8e4ea-a03e-16b5-9b3a-7747b85e903c>
2011-05-26 14:54:07 +0000

    aggiornato SoggettoFactory.cs (mancava Soggetto)

--- trunk/team19/Model/SoggettoFactory.cs
+++ trunk/team19/Model/SoggettoFactory.cs
@@ -20,6 +20,52 @@ namespace Team19.Model
         {
             return new ClienteFornitore(denominazione, telefono, email, partitaIva,codiceFiscale, indirizzo);
         }
+        public abstract class Soggetto
+        {
+            private string _denominazione;
+            private string _telefono;
+            private string _eMail;
+            private string _partitaIva;
+            private Indirizzo _indirizzo;
+            protected Soggetto(string denominazione, string telefono, string email, string partitaIva, Indirizzo indirizzo)
+            {
+                _denominazione = denominazione;
+                _telefono = telefono;
+                _eMail = email;
+                _partitaIva = partitaIva;
+                _indirizzo = indirizzo;
+            }
+
+            public Indirizzo Indirizzo
+            {
+                get { return _indirizzo; }
+                //set { _indirizzo = value; }
+            }
+
+            public string Telefono
+            {
+                get { return _telefono; }
+                //set { _telefono = value; }
+            }
+
+            public string EMail
+            {
+                get { return _eMail; }
+                //set { _eMail = value; }
+            }
+
+            public string Denominazione
+            {
+                get { return _denominazione; }
+                //set { _denominazione = value; }
+            }
+
+            public string PartitaIva // Chiedere per il null
+            {
+                get { return _partitaIva; }
+                //set { _partitaIva = value; }
+            }
+        }
         public class Cliente : Soggetto, ICliente
         {
 
