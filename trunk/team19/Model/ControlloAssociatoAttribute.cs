009134950923211dde559deb7dfd952cc1d75d4a
valep27 <valep27@acc8e4ea-a03e-16b5-9b3a-7747b85e903c>
2011-06-04 15:49:43 +0000

    Aggiunto ControlloAssociatoAttribute.cs

---
+++ trunk/team19/Model/ControlloAssociatoAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Team19.Model
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
+    class ControlloAssociatoAttribute : System.Attribute
+    {
+        private Control _controllo;
+
+        public ControlloAssociatoAttribute(Control controllo)
+        {
+            _controllo = controllo;
+        }
+
+        public Control Controllo
+        {
+            get { return _controllo; }
+            set { _controllo = value; }
+        }
+
+
+
+    }
+}
