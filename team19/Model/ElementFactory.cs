59e9690bb403db1237417e838340d96296952b03
valep27 <valep27@acc8e4ea-a03e-16b5-9b3a-7747b85e903c>
2011-06-05 15:50:44 +0000

    aggiunto ElementFactory.cs

---
+++ team19/Model/ElementFactory.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Team19.Model
+{
+    static class ElementFactory
+    {
+        #region Fatture
+        public static FatturaVendita CreateFatturaVendita(Cliente cliente, DateTime data, List<RigaFattura> elencoProdotti)
+        {
+            return FatturaVendita.CreateFatturaVendita(cliente, data, elencoProdotti);
+        }
+
+        public static FatturaAcquisto CreateFatturaAcquisto(Fornitore fornitore, DateTime data, int numero, Currency importo)
+        {
+            return FatturaAcquisto.CreateFatturaAcquisto(fornitore, data, numero, importo);
+        }
+        #endregion
+
+        #region Soggetti
+        public static Cliente CreateCliente(string denominazione, string telefono, string email, string partitaIva, string codiceFiscale, Indirizzo indirizzo)
+        {
+            return new Cliente(denominazione, telefono, email, partitaIva, codiceFiscale, indirizzo);
+        }
+
+        public static Fornitore CreateFornitore(string denominazione, string telefono, string email, string partitaIva, Indirizzo indirizzo)
+        {
+            return new Fornitore(denominazione, telefono, email, partitaIva, indirizzo);
+        }
+        #endregion
+
+        #region Movimenti
+        public static MovimentoDiDenaro CreateInteresseBancario(DepositoDiDenaro deposito, double interesse, DateTime data, Dipendente dipendente, string causale)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static MovimentoDiDenaro CreateSpesaBancaria(DepositoDiDenaro deposito, Currency importo, DateTime data, Dipendente dipendente, string causale)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static MovimentoDiDenaro CreatePagamentoAcquisto(ISorgente sorgente, IDestinazione destinazione, DateTime data, Dipendente dipendente, string causale)
+        {
+            return MovimentoFactory.CreatePagamentoAcquisto(sorgente, destinazione, data, dipendente, causale);
+        }
+
+        public static MovimentoDiDenaro CreateIncassoVendita(FatturaVendita sorgente, IDestinazione destinazione, DateTime data, Dipendente dipendente, string causale)
+        {
+            return MovimentoFactory.CreateIncassoVendita(sorgente, destinazione, data, dipendente, causale);
+        }
+
+        public static MovimentoDiDenaro CreateMovimentoInterno(Cassa sorgente, DepositoDiDenaro destinazione, Currency importo, DateTime data, Dipendente dipendente, string causale)
+        {
+            return MovimentoFactory.CreateMovimentoInterno(sorgente, destinazione, importo, data, dipendente, causale);
+        }
+
+        public static MovimentoDiDenaro CreateMovimentoInterno(DepositoDiDenaro sorgente, Cassa destinazione, Currency importo, DateTime data, Dipendente dipendente, string causale)
+        {
+            return MovimentoFactory.CreateMovimentoInterno(sorgente, destinazione, importo, data, dipendente, causale);
+        }
+
+        public static MovimentoDiDenaro CreateMovimentoInterno(DepositoDiDenaro sorgente, DepositoDiDenaro destinazione, Currency importo, DateTime data, Dipendente dipendente, string causale)
+        {
+            return MovimentoFactory.CreateMovimentoInterno(sorgente, destinazione, importo, data, dipendente, causale);
+        }
+        #endregion
+
+    }
+}
