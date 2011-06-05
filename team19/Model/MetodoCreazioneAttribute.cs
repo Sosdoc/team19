a03a91f65b8fca2c38235ebd41e2efcf6825b509
francesco.casimiro <francesco.casimiro@acc8e4ea-a03e-16b5-9b3a-7747b85e903c>
2011-06-05 17:31:59 +0000

    Sistemato InsertForm, popolato dai parametri del metodo di creazione nella ElementFactory

---
+++ team19/Model/MetodoCreazioneAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace Team19.Model
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor, Inherited = true, AllowMultiple = false)]
+    class MetodoCreazioneAttribute:System.Attribute
+    {
+        private string _tipo;
+        private Type[] _controlli;
+
+        
+        public MetodoCreazioneAttribute(string tipo,Type[] controlli)
+        {
+            _tipo = tipo;
+            foreach (Type t in controlli)
+            {
+                if (!t.IsSubclassOf(typeof(Control))) throw new ArgumentException();
+
+            }
+            _controlli = controlli;
+        }
+
+        public string Tipo
+        {
+            get { return _tipo; }
+            set { _tipo = value; }
+        }
+        public Type[] Controlli
+        {
+            get { return _controlli; }
+            set { _controlli = value; }
+        }
+
+    }
+}
